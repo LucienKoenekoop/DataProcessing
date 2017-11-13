b0fd399faf940f3e8e2457a8dada21318fb32f70
LucienKoenekoop <lucien.koenekoop@gmail.com>
2017-11-13 14:23:24 +0100

    V3.2
    V3.2

--- Homework/Week3/convertCSV2JSON.py
+++ Homework/Week3/convertCSV2JSON.py
@@ -5,11 +5,15 @@ This script reformats a CSV dataset to a JSON dataset
 import csv
 import json
 
+# opens the csv file for reading and the json file for writing
 csvFile = open('uefa.csv', 'r')
 jsonFile = open('uefa.json', 'w')
 
-columns = ("Country", "Pts")
-reader = csv.DictReader(csvFile, columns)
+# creates labels for the csv's
+labels = ("Country", "Pts")
+reader = csv.DictReader(csvFile, labels)
+
+# writes the json file
 for row in reader:
 	json.dump(row, jsonFile)
 	jsonFile.write('\n')
