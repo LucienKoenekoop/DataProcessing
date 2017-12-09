4a4b6d706387fec1dcb5d369a0163d2ffb1492d9
LucienKoenekoop <lucien.koenekoop@gmail.com>
2017-12-09 23:26:21 +0100

    V6.2
    V6.2

--- Homework/Week6/convertCSV2JSON.py
+++ Homework/Week6/convertCSV2JSON.py
@@ -6,11 +6,11 @@ import csv
 import json
 
 # opens the csv file for reading and the json file for writing
-csvFile = open('renewable.csv', 'r')
-jsonFile = open('renawable.json', 'w')
+csvFile = open('energy200405.csv', 'r')
+jsonFile = open('energy200415.json', 'w')
 
 # creates labels for the csv's
-labels = ("Country", "Energy", "Biofuels", "Hydropower", "Wind", "Solar", "Geothermal")
+labels = ("Country", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015")
 reader = csv.DictReader(csvFile, labels)
 data = []
 
--- Homework/Week6/linked.js
+++ Homework/Week6/linked.js
@@ -8,6 +8,40 @@ Week 6
 
 window.onload = function () {
 
+var width2 = 200;
+
+var x2 = d3.scale.linear()
+    .domain([1, 100])
+    .range([0, width])
+    .clamp(true);
+
+var dispatch = d3.dispatch("sliderChange");
+
+var slider = d3.select(".slider")
+    .style("width", width2 + "px");
+
+var sliderTray = slider.append("div")
+    .attr("class", "slider-tray");
+
+var sliderHandle = slider.append("div")
+    .attr("class", "slider-handle");
+
+sliderHandle.append("div")
+    .attr("class", "slider-handle-icon")
+
+slider.call(d3.behavior.drag()
+    .on("dragstart", function() {
+      dispatch.sliderChange(x2.invert(d3.mouse(sliderTray.node())[0]));
+      d3.event.sourceEvent.preventDefault();
+    })
+    .on("drag", function() {
+      dispatch.sliderChange(x2.invert(d3.mouse(sliderTray.node())[0]));
+    }));
+
+dispatch.on("sliderChange.slider", function(value) {
+  sliderHandle.style("left", x2(value) + "px")
+});
+
 			var countries = ["#BE", "#BG", "#CZ", "#DK", "#DE", "#EE", "#IE", "#GR", "#ES", "#FR", 
 							 "#HR", "#IT", "#CY", "#LV", "#LT", "#LU", "#HU", "#MA", "#NL", "#AT",
 							 "#PL", "#RO", "#SI", "#SK", "#FI", "#SE", "#GB", "#IS", "#NO", "#AL",
@@ -15,8 +49,9 @@ window.onload = function () {
 
 										  // set the margin range to fit the axis and values
 							  var margin = {top: 20, right: 20, bottom: 80, left: 40},
-							      width = 600 - margin.left - margin.right,
-							      height = 300 - margin.top - margin.bottom;
+							      width = 800 - margin.left - margin.right,
+							      height = 300 - margin.top - margin.bottom,
+							      currentCountry;
 
 							  // set the ranges
 							  var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
@@ -52,13 +87,13 @@ window.onload = function () {
 							  // svg.call(tip);
 				
 			queue()
-				.defer(d3.csv, 'energy200405.csv')
+				.defer(d3.json, 'energy200415.json')
 				.defer(d3.csv, 'renewable.csv')
 				.defer(d3.xml, 'europe.svg')
 				.defer(d3.json, 'uefa.json')
 				.await(makeMyMap);
 
-			function makeMyMap(error, csv1, csv2, xml, json) {
+			function makeMyMap(error, json1, csv2, xml, json2) {
 				if (error) throw error;
 
 				document.body.appendChild(xml.documentElement);
@@ -84,11 +119,54 @@ window.onload = function () {
 
 					for (i = 0; i <= countries.length - 1; i++) {
 						if (country[0][0].id == (csv2[i].Country)) {
-							console.log(csv2[i].Country)
-						}	
-					}
-				}
+							console.log(csv2[i]);
+							currentCountry = csv2[i];
 
+
+							console.log(currentCountry);
+							xLabels = ["Energy", "Biofuels", "Hydropower", "Wind", "Solar", "Geothermal"];
+							x.domain([xLabels]);
+							y.domain([0, currentCountry.Energy]);
+
+						    svg.append("g")
+						        .attr("class", "x axis")
+						        .attr("transform", "translate(0," + height + ")")
+						        .call(xAxis)
+						      .selectAll("text")
+						        .style("text-anchor", "start")
+						        .attr("dx", "-.8em")
+						        .attr("dy", "-.55em")
+						        .attr("transform", "translate(0,15)");
+
+				    		svg.append("g")
+						        .attr("class", "y axis")
+						        .call(yAxis)
+						      .append("text")
+						        .attr("transform", "rotate(-90)")
+						        .attr("x", -10)
+						        .attr("y", -40)
+						        .attr("dy", ".71em")
+						        .style("text-anchor", "end")
+						        .text("Energy");
+
+						    svg.selectAll("bar")
+						        .data(csv2)
+						      .enter().append("rect")
+						        .attr("id", "bar2")
+						        .attr("x", function(d) { return xLabels })
+						        .attr("width", x.rangeBand())
+						        .attr("y", function(d) { return y(d[2015]); })
+						        .attr("height", function(d) { return height - y(d[2015]); });
+						};	
+					};
+
+					var active = bar.active ? false : true,
+						newOpacity = active ? 0 : 1;
+					d3.selectAll("#bar").style("opacity", newOpacity);
+					d3.select("#xAxis").style("opacity", newOpacity);
+					d3.select("#yAxis").style("opacity", newOpacity);
+					bar.active = active;
+				}
 
 				for (i = 0; i <= countries.length - 1; i++) {
 
@@ -103,43 +181,47 @@ window.onload = function () {
 
 
 									// scale the range of the data
-								    x.domain(json.map(function(d) { return d.Country; }));
-								    y.domain([0, d3.max(json, function(d) { return d.Pts; })]);
+								    x.domain(json1.map(function(d) { return d.Country; }));
+								    y.domain([0, 100]);
+
+								    console.log(json1.map(function(d) { return d.Country; }))
 
 								    // add x-axis with countries indicating the bars
 								    svg.append("g")
+								    	.attr("id", "xAxis")
 								        .attr("class", "x axis")
 								        .attr("transform", "translate(0," + height + ")")
 								        .call(xAxis)
 								      .selectAll("text")
-								        .style("text-anchor", "end")
+								        .style("text-anchor", "start")
 								        .attr("dx", "-.8em")
 								        .attr("dy", "-.55em")
-								        .attr("transform", "rotate(-45)");
+								        .attr("transform", "translate(0,15)");
 
 								    // add y-axis with points values for the bars
 								    svg.append("g")
+								    	.attr("id", "yAxis")
 								        .attr("class", "y axis")
 								        .call(yAxis)
 								      .append("text")
 								        .attr("transform", "rotate(-90)")
+								        .attr("x", -10)
 								        .attr("y", -40)
 								        .attr("dy", ".71em")
 								        .style("text-anchor", "end")
-								        .text("Points");
+								        .text("Energy");
 
 								    // Add bar chart and mouse hover events
 								    svg.selectAll("bar")
-								        .data(json)
+								        .data(json1)
 								      .enter().append("rect")
-								        .attr("class", "bar")
+								        .attr("id", "bar")
 								        .attr("x", function(d) { return x(d.Country); })
 								        .attr("width", x.rangeBand())
-								        .attr("y", function(d) { return y(d.Pts); })
-								        .attr("height", function(d) { return height - y(d.Pts); });
+								        .attr("y", function(d) { return y(d[2015]); })
+								        .attr("height", function(d) { return height - y(d[2015]); });
 								        // .on('mouseover', tip.show)
 								        // .on('mouseout', tip.hide);
-
 
 			}
 }
